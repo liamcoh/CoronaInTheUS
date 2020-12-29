6fc03e731abf7b555653b01a0185fb2843fa9b62
Liam Cohen <LCmanutd@gmail.com>
2020-12-29 15:23:09 +0200

    Fetching data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useCallback, useState} from "react"
+import StateList from './Components/StatesList'
+import DatePicker from "react-datepicker";
 
 function App() {
+  const [date, setDate] = useState(new Date())
+  const [state, setState] = useState('TX')
+  const [states, setStates] = useState([])
+  const [positive, setPos] = useState(0)
+  const [negative, setNeg] = useState(0)
+  const minDate = new Date(2020, 2, 6)
+
+  const getStates = useCallback(() => {
+      fetch(`https://api.covidtracking.com/v1/states/info.json`)
+          .then(response => response.json())
+          .then(data => {
+              let arr = []
+              data.forEach(ele => {
+                arr.push(ele.state)
+              });
+              setStates(arr)
+          })
+  }, [])
+  useEffect(getStates, []) // eslint-disable-line react-hooks/exhaustive-deps
+
+  useEffect((() => {
+    fetch(`https://api.covidtracking.com/v1/states/${state}/daily.json`)
+        .then(response => response.json())
+        .then(data => {
+          console.log(data)
+          data.forEach(ele => {
+            let strDate = ele.date.toString()
+            if(parseInt(strDate.substring(0,4)) === date.getFullYear()){
+              if(parseInt(strDate.substring(4,6)) === (date.getMonth()+1)){
+                if(parseInt(strDate.substring(6,8)) === date.getUTCDate()){
+                  setPos(ele.positiveIncrease)
+                  setNeg(ele.negativeIncrease)
+                }
+              }
+            }
+          })
+        })
+  }), [date, state]) 
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <div>
+        <DatePicker selected={date} onChange={setDate} minDate={minDate} />
+        <StateList set={setState} list={states} />
+        <h1>state: {state.toString()}</h1>
+        <h1>date: {date.getMonth()+1}/{date.getUTCDate()}</h1>
+        <h1>positive: {positive}</h1>
+        <h1>negative: {negative}</h1>
+      </div>
+  )
 }
 
-export default App;
+export default App
