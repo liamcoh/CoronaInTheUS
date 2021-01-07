71a1e64180a2eee7c1b87479c22d98e945ed5c0d
Liam Cohen <LCmanutd@gmail.com>
2021-01-07 10:45:09 +0200

    final

--- src/Components/HistoryList.js
+++ src/Components/HistoryList.js
@@ -1,11 +1,8 @@
 import React from 'react'
 import '../App.css'
 import deleteIcon from '../Assets/delete-button.svg'
-const { v4: uuidv4 } = require('uuid');
 
 export default React.memo(({ queue, addQueue }) => {
-    
-    const items = []
 
     function removeLine(ele){
         let tmp = [...queue]
@@ -13,26 +10,29 @@ export default React.memo(({ queue, addQueue }) => {
         addQueue(tmp)
     }
     
-    queue.forEach(ele => {
-        let key = uuidv4()
-        items.push(<div className='flexBox'
-            key={key} 
-            style={{ color: ele.color, fontWeight: ele.weight }}>
-                <div className='flexItem'>מדינה:{ele.name}</div>
-                <div className='flexItem'>תאריך:{ele.date}</div>
-                <div className='flexItem'>חיוביים:{ele.pos}</div>
-                <div className='flexItem'>שליליים:{ele.neg}</div>
-                <button 
-                style={{ width: '5%', height: '5%', float: 'left' }} 
-                className='button' 
-                onClick={() => removeLine(ele)}>
-                    <img className='img' src={deleteIcon} alt='delete'></img>
-                </button>
-            </div>)})
+    function getRows() {
+        return queue.map((ele, index) => {
+        return <div className='flexBox'
+        key={ele + index} 
+        style={{ color: ele.color, fontWeight: ele.weight }}>
+            <div className='flexItem'>מדינה:{ele.name}</div>
+            <div className='flexItem'>תאריך:{ele.date}</div>
+            <div className='flexItem'>חיוביים:{ele.pos}</div>
+            <div className='flexItem'>שליליים:{ele.neg}</div>
+            <button 
+            style={{ width: '5%', height: '5%', float: 'left' }} 
+            className='button' 
+            onClick={() => removeLine(ele)}>
+                <img className='img' src={deleteIcon} alt='delete'></img>
+            </button>
+        </div>
+        })
+    }
+
 
     return (
         <div>
-            {items}
+            {getRows()}
         </div>
     )
   })
